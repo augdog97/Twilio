df563c04902bc591df253e2b9bfc794d23b0292c
augdog97 <augdog@me.com>
2020-11-14 00:00:10 -0500

    Rendered api test data on page, Twilio url doesnt work

--- routes/index-router.js
+++ routes/index-router.js
@@ -1,13 +1,16 @@
+
+/* Imports */
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+/* .env import */
 require('dotenv').config();
 
 /* This parses the JSON from the AJAX req */
 router.use(bodyParser.json());
-
-
 
 
 /* Inputing the API keys for Twilio, they are in a .env file */
@@ -16,18 +19,33 @@ var authToken = process.env.AUTH_TOKEN;   // Your Auth Token from www.twilio.com
 
 /* Creating boilerplate Twilio */
 var twilio = require('twilio');
+const { data } = require('jquery');
 var client = new twilio(accountSid, authToken); 
 
 
 /* GET home page */
-router.get('/',  (req, res, next) => {
-    try {
-      res.render('index');  
-    } catch (error) {
-      res.send(error);
-    } 
+router.get('/',  async (req, res, next) => {
+  const url_api = 'https://api.twilio.com/2010-04-01/Accounts/{Account Sid}/Messages/{Message Id}';
+ 
+ 
+  /* Test */
+  const test = 'https://httpbin.org/get'
+
+  try {
+    await fetch(test)
+      .then(res => res.json())
+      .then(data => {
+        const ip = data.origin;
+        console.log(ip);
+        res.render('index', {
+          ip: ip
+        }); 
+      })
+  } catch (error) {
+    console.log(error);
+  }
     
-   next()
+ 
 }); 
 
 
@@ -35,7 +53,8 @@ router.get('/',  (req, res, next) => {
   1. Create a message on Twilio and send the message
   2. The body is JSON from the AJAX request 
   */
-router.post('/',  async (req, res, next) => {
+router.post('/',   (req, res, next) => {
+    
       client.messages.create({
         body: `${req.body.message}`,
         to: `${req.body.number}`,  // Text this number
@@ -43,17 +62,13 @@ router.post('/',  async (req, res, next) => {
     })
         .then((message) => console.log(message.sid)); 
 
-  // Access the message body and the number it was sent from.
-  await console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
-    const message = req.body.Body;
-    res.render('index', {
-      message: message
-    })
+    next();
+     
+ 
 }) 
 
-
-
-
-
+router.post('/', async (req,res) => {
+  const messageBody = req.body.Body;
+})
 
 module.exports = router;
